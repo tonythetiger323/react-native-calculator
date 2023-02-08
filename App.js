7f086e9ce3bc2cfaa9ee7a809336b8ee380b95ba
Tony Greeley <tony.greeley@me.com>
2023-02-07 18:38:12 -0600

    Completed button layout of calculator with button styling.

--- App.js
+++ App.js
@@ -5,7 +5,35 @@ import Button from './components/UI/Button';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Button text='1' onPress={() => {}} />
+      <View style={styles.row}>
+        <Button color='light' text='C' onPress={() => {}} />
+        <Button color='light' text='+/-' onPress={() => {}} />
+        <Button color='light' text='%' onPress={() => {}} />
+        <Button color='accent' text='/' onPress={() => {}} />
+      </View>
+      <View style={styles.row}>
+        <Button text='7' onPress={() => {}} />
+        <Button text='8' onPress={() => {}} />
+        <Button text='9' onPress={() => {}} />
+        <Button color='accent' text='x' onPress={() => {}} />
+      </View>
+      <View style={styles.row}>
+        <Button text='4' onPress={() => {}} />
+        <Button text='5' onPress={() => {}} />
+        <Button text='6' onPress={() => {}} />
+        <Button color='accent' text='-' onPress={() => {}} />
+      </View>
+      <View style={styles.row}>
+        <Button text='1' onPress={() => {}} />
+        <Button text='2' onPress={() => {}} />
+        <Button text='3' onPress={() => {}} />
+        <Button color='accent' text='+' onPress={() => {}} />
+      </View>
+      <View style={styles.row}>
+        <Button size='2' text='0' onPress={() => {}} />
+        <Button text='.' onPress={() => {}} />
+        <Button color='accent' text='=' onPress={() => {}} />
+      </View>
       <StatusBar style='auto' />
     </View>
   );
@@ -17,5 +45,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#252525',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
   },
 });
--- components/UI/Button.js
+++ components/UI/Button.js
@@ -1,25 +1,12 @@
 import { Pressable, Text, StyleSheet, Dimensions } from 'react-native';
 
-const Button = ({ text, onPress }) => {
-  return (
-    <Pressable
-      onPress={onPress}
-      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
-    >
-      <Text style={styles.text}>{text}</Text>
-    </Pressable>
-  );
-};
-
-export default Button;
-
 const screenWidth = Dimensions.get('window').width;
 const buttonWidth = screenWidth / 4;
 
 const styles = StyleSheet.create({
   button: {
+    flex: 1,
     backgroundColor: '#414141',
-    width: buttonWidth,
     height: Math.floor(buttonWidth - 10),
     borderRadius: Math.floor(buttonWidth),
     alignItems: 'center',
@@ -30,5 +17,50 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
+    fontSize: 36,
+  },
+  darkText: {
+    color: '#333',
+  },
+  light: {
+    backgroundColor: '#d3d3d3',
+  },
+  accent: {
+    backgroundColor: '#f5a122',
+  },
+  doubleWidth: {
+    width: buttonWidth * 2,
+    flex: 0,
+    alignItems: 'flex-start',
+    paddingLeft: 40,
   },
 });
+
+const Button = ({ color, size, text, onPress }) => {
+  let buttonStyles = [styles.button];
+  const textStyles = [styles.text];
+
+  if (color === 'light') {
+    buttonStyles.push(styles.light);
+    textStyles.push(styles.darkText);
+  }
+
+  if (color === 'accent') {
+    buttonStyles.push(styles.accent);
+  }
+
+  if (size === '2') {
+    buttonStyles.push(styles.doubleWidth);
+  }
+
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => [buttonStyles, pressed && styles.pressed]}
+    >
+      <Text style={textStyles}>{text}</Text>
+    </Pressable>
+  );
+};
+
+export default Button;
